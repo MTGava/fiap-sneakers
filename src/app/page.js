4a2153cf5fe1db6b150b60a1ed0c83299af07ba4
Matheus Gava Silva <RM89113@fiap.com.br>
2023-08-15 22:46:06 -0300

    ajustando navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,15 @@
 export default function Home() {
   return (
-    <nav>
-      <ul>
+    <nav className="flex p-4 bg-neutral-900">
+      <ul className="flex gap-20">
         <li>
           <a href="#">
             <h1>Fiap Filmes</h1>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <h1>filmes</h1>
           </a>
         </li>
       </ul>
